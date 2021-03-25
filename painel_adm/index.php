d5d21c298a334f259da9a157e7322307f5ac84d8
exebixel <ezequielnat7@gmail.com>
2021-03-24 22:41:01 -0300

    added login screen

---
+++ painel_adm/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/login.css" type="text/css" media="screen" charset="utf-8">
+    <title>Painel Administrativo</title>
+  </head>
+  <body>
+    <div class="login-page">
+      <div class="form">
+
+        <form class="login-form">
+          <input type="text" placeholder="email"/>
+          <input type="password" placeholder="senha"/>
+          <button>Login</button>
+        </form>
+      </div>
+    </div>
+
+  </body>
+</html>
