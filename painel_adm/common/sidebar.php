7001a20e3e0185d061b6c5791df695cee9bf3dfc
exebixel <ezequielnat7@gmail.com>
2021-04-05 10:10:58 -0300

    delete user.php reference in sidebar

--- painel_adm/common/sidebar.php
+++ painel_adm/common/sidebar.php
@@ -10,12 +10,6 @@
   <div class="sidebar-wrapper">
     <ul class="nav">
       <li class="nav-item ">
-        <a class="nav-link" href="<?= PATH_ROOT ?>/user.php">
-          <i class="material-icons">person</i>
-          <p>User Profile</p>
-        </a>
-      </li>
-      <li class="nav-item ">
         <a class="nav-link" href="<?= PATH_ROOT ?>/noticia/index.php">
           <i class="material-icons">content_paste</i>
           <p>Tabelas de Notícias</p>
