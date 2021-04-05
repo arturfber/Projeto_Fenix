e5408f01a887fa2b979b2b25879fd296d7cace08
exebixel <ezequielnat7@gmail.com>
2021-04-05 10:02:47 -0300

    added noticia folder

---
+++ painel_adm/noticia/salvarcadastro.php
@@ -0,0 +1,16 @@
+<?php
+
+    include_once "../blog/conexao.php";
+
+    $titulo = $_POST['titulo'];
+    $subtitulo = $_POST['subtitulo'];
+    $texto = $_POST['texto'];
+    $data_noticia = $_POST['data_noticia'];
+    $autor = $_POST['autor'];
+    $categoria = $_POST['categoria'];
+
+    $sql = 'insert into noticias (titulo, subtitulo, texto, data_noticia, autor, fk_categoria_id ) values ("' . $titulo . '","' . $subtitulo . '","' . $texto . '","' . $data_noticia . '","' . $autor . '","' . $categoria . '") ';
+
+    $connect->exec($sql);
+
+    header('location: tables.php');
---
+++ painel_adm/noticia/tables.php
@@ -0,0 +1,121 @@
+<?php
+
+  include_once "../blog/conexao.php";
+
+  $sql = 'select * from noticias';
+  $not = $connect->query($sql);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt">
+
+
+<head>
+  <?php
+    include_once "common/head.php";
+  ?>
+</head>
+
+<body class="">
+  <div class="wrapper ">
+
+    <?php
+      include_once "common/sidebar.php";
+    ?>
+
+    <div class="main-panel">
+      <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
+        <div class="container-fluid">
+          <div class="navbar-wrapper">
+            <a class="navbar-brand" href="javascript:;">Tabelas de Notícias</a>
+          </div>
+        </div>
+      </nav>
+      <!-- End Navbar -->
+
+      <div class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card">
+                <div class="card-header card-header-primary">
+                  <h4 class="card-title ">NOTÍCIAS DE GAMES</h4>
+                  <p class="card-category"> Todas as notícias cadastradas</p>
+                </div>
+                <div class="card-body">
+                  <div class="table-responsive">
+                    <table class="table">
+                      <thead class=" text-primary">
+                  
+                      <th>
+                          NOTÍCIAS
+                      </th>
+                      <th>
+                          EDITAR
+                      </th>
+
+                      <th>
+                          EXCLUIR
+                      </th>
+                      </thead>  
+                      
+                        <?php
+                          foreach($not as $noticia):
+                        ?>
+    
+                        <tbody>
+                          <tr>
+                            <td>
+                              <?=$noticia['titulo']?>
+                            </td>
+                            <td class="text-primary">
+                              <a href="">0</a>
+                            </td>
+                            <td class="text-primary">
+                              <a href="">X</a>
+                            </td>
+                          </tr>
+
+                        <?php
+                          endforeach;
+                        ?>
+                        
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+                <div class="card-body">
+                  <div class="table-responsive">
+                    <table class="table">
+                      <thead class=" text-primary">
+                      <th>
+                          <a href="cadastro.php">CADASTRAR NOVA NOTÍCIA</a>
+                      </th>
+                      </thead>  
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <?php
+        include_once "common/footer.php";
+      ?>
+
+    </div>
+  </div>
+
+  <?php
+    include_once "common/scripts.php";
+  ?>
+
+</body>
+
+</html>
