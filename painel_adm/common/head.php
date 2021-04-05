a4b1894b667fe857f772f9e959663dc447cf492e
exebixel <ezequielnat7@gmail.com>
2021-04-05 17:53:03 -0300

    fix reference in head.php

--- painel_adm/common/head.php
+++ painel_adm/common/head.php
@@ -10,4 +10,4 @@
 <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
 <!-- CSS Files -->
-<link href="http://localhost/~exebixel/Projeto_Fenix/painel_adm/assets/css/material-dashboard.css?v=2.1.2" rel="stylesheet" />
+<link href="<?= PATH_ROOT ?>/assets/css/material-dashboard.css?v=2.1.2" rel="stylesheet" />
